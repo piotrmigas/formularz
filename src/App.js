197353a05badf9339f7ca37ada874f41ffb31c24
piotrmigas <piotr.migas@icloud.com>
2021-10-21 12:54:14 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { useFormik, Field, Form } from "formik";
+import * as yup from "yup";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: theme.spacing(2),
+
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+    },
+    "& .MuiButtonBase-root": {
+      margin: theme.spacing(2),
+    },
+  },
+  row: {
+    display: "flex",
+  },
+}));
 
 function App() {
+  const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      password: "",
+      confirmPassword: "",
+      //terms: false,
+    },
+    validationSchema: yup.object({
+      firstName: yup.string().max(15, "Imię może mieć max 15 znaków").required("Proszę podać imię"),
+      lastName: yup.string().max(20, "Nazwisko może mieć max 15 znaków").required("Proszę podać nazwisko"),
+      email: yup.string().email("Nieprawidłowy email").required("Proszę podać email"),
+      phone: yup.string().matches(phoneRegExp, "Nieprawidłowy numer telefonu").required("Proszę podać numer telefonu"),
+      password: yup.string().min(8, "Hasło musi mieć min 8 znaków").required("Proszę podać hasło"),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "Hasła muszą się zgadzać")
+        .required("Proszę potwierdzić hasło"),
+      //terms: yup.boolean().required().oneOf([true], "Proszę zaakceptować warunki"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Dialog open={true}>
+      <form onSubmit={formik.handleSubmit} className={classes.root}>
+        <div>
+          <TextField
+            label="Imię"
+            variant="filled"
+            name="firstName"
+            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+            helperText={formik.touched.firstName && formik.errors.firstName}
+            autoComplete="off"
+          />
+          <TextField
+            label="Nazwisko"
+            variant="filled"
+            {...formik.getFieldProps("lastName")}
+            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+            helperText={formik.touched.lastName && formik.errors.lastName}
+            autoComplete="off"
+          />
+        </div>
+        <div className={classes.row}>
+          <TextField
+            label="Email"
+            variant="filled"
+            type="email"
+            {...formik.getFieldProps("email")}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            autoComplete="off"
+          />
+          <TextField
+            label="Telefon"
+            variant="filled"
+            {...formik.getFieldProps("phone")}
+            error={formik.touched.phone && Boolean(formik.errors.phone)}
+            helperText={formik.touched.phone && formik.errors.phone}
+            autoComplete="off"
+          />
+        </div>
+        <div className={classes.row}>
+          <TextField
+            label="Hasło"
+            variant="filled"
+            type="password"
+            {...formik.getFieldProps("password")}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+            autoComplete="off"
+          />
+          <TextField
+            label="Potwierdź hasło"
+            variant="filled"
+            type="password"
+            {...formik.getFieldProps("confirmPassword")}
+            error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+            helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+            autoComplete="off"
+          />
+        </div>
+        {/* <FormControlLabel
+          control={<Field component={Checkbox} type="checkbox" name="terms" />}
+          label="I agree with terms and conditions"
+        /> */}
+        <div>
+          <Button variant="contained">Anuluj</Button>
+          <Button type="submit" variant="contained" color="primary">
+            Zarejestruj
+          </Button>
+        </div>
+      </form>
+    </Dialog>
   );
 }
 
---
+++ src/redux/dataSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const slice = createSlice({
+  name: "data",
+  initialState: {},
+  reducers: {},
+  extraReducers: {},
+});
+
+export default slice.reducer;
